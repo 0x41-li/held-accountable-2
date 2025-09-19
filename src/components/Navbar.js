6a26886c37e5286314ddc2951f968669679363e1
Bogdan <bogdan@vtslabs.com>
2025-09-19 09:27:07 -0400

    Fix Navbar z-index to show above the carousel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ export default function Navbar() {
         <Icon icon="mingcute:menu-fill" />
       </button>
       {show && (
-        <div className="fixed left-0 top-[60px] bottom-0 right-0 bg-[#000000cc] flex border-t z-[1]">
+        <div className="fixed left-0 top-[60px] bottom-0 right-0 bg-[#000000cc] flex border-t z-[999]">
           <div className="w-1/3" onClick={() => setShow(false)}></div>
           <div className="w-2/3 bg-white pt-[20px]">
             <div className="flex flex-col px-[16px] gap-[4px] h-full">
