efc652b93486edf31966b7d1fe81bb679eb552f0
avoid163 <artemzotov7@gmail.com>
2025-09-19 09:54:30 +0300

    WIP

---
+++ src/app/snapshots/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Snapshots() {
+  return (
+    <div className='flex flex-col mt-3 border-l border-t border-[#E4E7EC] rounded-tl-4xl h-full'>
+      ddd
+    </div>
+  )
+}
