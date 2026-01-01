4a43ea73074f49d2ea1cc9de70a946aa26a8abc9
Ali Sbane <ali.sbane.dev@gmail.com>
2026-01-01 01:46:10 +0100

    Changes to make the build without error, more specific is ignoring the needed keys

--- src/app/app/subscription/page.js
+++ src/app/app/subscription/page.js
@@ -1,19 +1,27 @@
 import Subscription from "@/views/Subscription";
 import { stripe } from "../../../../lib/stripe";
 
+export const dynamic = 'force-dynamic';
+
 export default async function SubscriptionPage() {
+  let clientSecret1 = null;
+  let clientSecret2 = null;
+
+  if (stripe) {
     // Create PaymentIntent as soon as the page loads
-    const { client_secret: clientSecret1 } = await stripe.paymentIntents.create({
+    const { client_secret: cs1 } = await stripe.paymentIntents.create({
       amount: 500,
       currency: 'usd',
       payment_method_types: ['card'],
-    })
-    const { client_secret: clientSecret2 } = await stripe.paymentIntents.create({
+    });
+    const { client_secret: cs2 } = await stripe.paymentIntents.create({
       amount: 5000,
       currency: 'usd',
       payment_method_types: ['card'],
-    })
+    });
+    clientSecret1 = cs1;
+    clientSecret2 = cs2;
+  }
 
-
-    return <Subscription clientSecret1={clientSecret1} clientSecret2={clientSecret2}/>;
+  return <Subscription clientSecret1={clientSecret1} clientSecret2={clientSecret2} />;
 }
