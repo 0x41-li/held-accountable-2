fba250cba4cc7dc9c5b82958c2c5185c8bbf75d0
Ali Sbane <ali.sbane.dev@gmail.com>
2026-01-02 02:27:17 +0100

    Premium modal to match the figma design'

--- src/views/Home.js
+++ src/views/Home.js
@@ -637,7 +637,7 @@ export default function Home() {
 
             {/* Left Column - Polls */}
             <div className={`flex-1 ${isMobile ? 'overflow-visible' : 'overflow-auto'} px-6 md:px-10 pt-[20px]`}>
-              <div className="flex flex-row lg:flex-col gap-6">
+              <div className="flex flex-col gap-6">
                 {polls.map((poll) => (
                   <Poll poll={poll} key={poll.id} showDetail={setDetailId} />
                 ))}
